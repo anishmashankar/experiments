8a424edaacbccae1fdb2c5b8c7c98480486552fe
Anish Mashankar <anishmashankar@users.noreply.github.com>
2015-06-17 00:33:17 +0530

    Update wikipedia.py

--- Python/wikipedia.py
+++ Python/wikipedia.py
@@ -16,24 +16,40 @@ import urllib2 #To download the web page data for extracting content
 import re #simple parsing on the extracted data is done using regular expression.
 
 a = raw_input("What do you want to know about: ") #taking a search query from user
+
+
 '''
 Okay, now the following step is quite important. When we see wikipedia URLs, it is
 observable that the words are not separated by a space, rather, an underscore '_'.
 This is what we are going to do here.
 '''
+
+
 c = a.replace(" ","_")
+
+
 #Let's download the web page!
 x = urllib2.urlopen("http://en.wikipedia.org/wiki/"+c).read()
+
+
 '''
 A BeautifulSoup object is initialized. It will help us wrangling the data we need
 '''
 soup = BeautifulSoup(x)
+
+
 '''
 Observing the wikipedia web page, all the content that exists in a tag
 'p' inside another one called 'mw-content-text'. This is how we are going to get there.
 '''
+
+
 content = soup.find(id='mw-content-text') #finds the part of HTML page with content
+
 p = content.find('p') #gets the first paragraph of the content
+
 g = p.get_text() #gets only the text of the paragraph without the HTML elements
+
 g = re.sub(r'\[\w+\]','', g)  #formatting off the extra tags that Wikipedia puts in for citations
+
 print g #print the data we got
